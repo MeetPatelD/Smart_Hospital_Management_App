0239033c7e1c6fe671fc7f6527cd8fffd9d4f432
MeetPatelD <meetpatel.engineers@gmail.com>
2026-04-03 00:20:48 +0530

    doctor.dart, doctor screen, patient screen, appointment dart

---
+++ lib/models/user.dart
@@ -0,0 +1,8 @@
+class User {
+  String name;
+  String email;
+  String password;
+  String role;
+
+  User({required this.name, required this.email, required this.password, required this.role});
+}
